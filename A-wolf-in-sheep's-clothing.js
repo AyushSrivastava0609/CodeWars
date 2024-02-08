3150ab48dcb80fce98033022e735867568f47099
Ayush Srivastava <ayushsrivastavawork@gmail.com>
2024-02-08 09:49:54 +0530

    Update A-wolf-in-sheep's-clothing.js

--- A-wolf-in-sheep's-clothing.js
+++ A-wolf-in-sheep's-clothing.js
@@ -1,1 +1,10 @@
-
+function warnTheSheep(queue) {
+let index=queue.indexOf("wolf")
+let pos=queue.pop()
+let pos1=queue.length-(index)
+if(pos=="wolf"){
+  return 'Pls go away and stop eating my sheep'
+}else{
+  return `Oi! Sheep number ${pos1}! You are about to be eaten by a wolf!`
+}
+}
